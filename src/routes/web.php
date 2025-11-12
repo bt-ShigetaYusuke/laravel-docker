5c6458e8f2c0531f7cf42da6773018e88429fb9e
bt-ShigetaYusuke <yusuke_shigeta@booklook.jp>
2025-11-12 14:56:49 +0900

    ルーティング

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * トップページ
+ */
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * /memos
+ * 
+ * Route::resource() : Laravelが「決まった7種類のURL」だけ自動生成する仕組み
+ * index, create, store, show, edit, update, destroy
+ * 
+ * その他のアクションを追加したら以下のようにルートも追加する
+ * 
+ * Route::get('memos/search', [MemoController::class, 'search'])->name('memos.search');
+ * → /memos/search にアクセスしたら → search()が実行されるって感じ
+ */
+Route::resource('memos', MemoController::class);
